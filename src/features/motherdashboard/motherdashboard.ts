9578c5bdb0376dadff4a1d2b536ba711c2935b9a
Leela Banda <leela.banda18@gmail.com>
2026-07-23 21:13:01 +0530

    first commit

--- src/features/motherdashboard/motherdashboard.ts
+++ src/features/motherdashboard/motherdashboard.ts
@@ -18,7 +18,7 @@ export class Motherdashboard {
 
 
   ngOnInit(){
-    console.log("Loaded the data");
+
     const data = localStorage.getItem('people');
 
     if(data){
@@ -54,27 +54,13 @@ export class Motherdashboard {
       if(!person.DOB){
         return false;
       }
-      console.log(
-  this.people.map(p => ({
-    name: p.Name,
-    anniversary: p.Anniversary
-  }))
-);
       const dob = this.convertDate(person.DOB);
       return next10Days.includes(
         `${dob.getDate()}-${dob.getMonth()}`
       );
     }).sort((a,b)=>{
       return this.convertDate(a.DOB).getTime()-this.convertDate(b.DOB).getTime();
-      
     });
-    console.log(
-  this.upCommingAnniversaries.map(p => ({
-    name: p.Name,
-    anniversary: p.Anniversary,
-    days: this.getDaysUntil(p.Anniversary!)
-  }))
-);
         this.upCommingAnniversaries = this.people.filter(person=>{
       if(person.Relation !== 'M-Friend' && person.Relation !== 'Relation'){
         return false;
@@ -92,46 +78,24 @@ export class Motherdashboard {
 
 
 
-// this.upCommingBirthdays = this.people
-//   .filter(person => {
-
-//     if (person.Relation !== 'M-Friend' &&
-//         person.Relation !== 'Relation') {
-//       return false;
-//     }
-
-//     if (!person.DOB) {
-//       return false;
-//     }
-
-//     const days = this.getDaysUntil(person.DOB);
-
-//     return days > 0 && days <= 10;
-//   })
-//   .sort((a, b) =>
-//     this.getDaysUntil(a.DOB) -
-//     this.getDaysUntil(b.DOB)
-//   );
+    // this.upCommingAnniversaries = this.people.filter(person=>{
+    //   if(person.Relation !== 'M-Friend' && person.Relation !== 'Relation'){
+    //     return false;
+    //   }
+    //   if(!person.Anniversary){
+    //     return false;
+    //   }
+    //   const anniversary = this.convertDate(person.Anniversary);
+    //   return next10Days.includes(
+    //     `${anniversary.getDate()}-${anniversary.getMonth()}`
+    //   );
+    //     }).sort((a,b)=>{
+    //   return this.convertDate(a.Anniversary!).getTime()-this.convertDate(b.Anniversary!).getTime();
+    // });
 
 
   }
-getDaysUntil(dateString: string): number {
 
-  const event = this.convertDate(dateString);
-  const today = new Date();
-
-  today.setHours(0, 0, 0, 0);
-  event.setHours(0, 0, 0, 0);
-
-  // If this year's event has passed, use next year
-  if (event.getTime() < today.getTime()) {
-    event.setFullYear(today.getFullYear() + 1);
-  }
-
-  return Math.floor(
-    (event.getTime() - today.getTime()) / (1000 * 60 * 60 * 24)
-  );
-}
 
 
   convertDate(value:string):Date{
